74ac83dca895445922db5c463d1ff126be42c853
mewo <177341604+lumilovesyou@users.noreply.github.com>
2025-09-17 16:31:34 -0500

    Easy app maker
    And i kept spelling "gifypet" as "giphypet" so i fixed that too

--- main.js
+++ main.js
@@ -1,12 +1,20 @@
 let highestZ = 1;
 
-//to-do: make an easy window maker function
-
 addEventListener("DOMContentLoaded", () => {
+    let windowPieces = [
+        ["https://linktree.lumiverse.dev/assets/images/ico-res.png", "Linktree", '<iframe src="https://linktree.lumiverse.dev/"></iframe>', "https://linktree.lumiverse.dev/", 50, 45],
+        ["./assets/images/gifypet/gifypet.png", "Gifypet", '<iframe scale="0.5" width="314" height="321" scrolling="no" src="https://gifypet.neocities.org/pet/pet.html?name=Mildew&dob=1758063721&gender=undefined&element=Water&pet=https%3A%2F%2Flumilovesyou.github.io%2Flumiverse%2Fassets%2Fimages%2Fgifypet%2Fmildew.gif&map=https%3A%2F%2Flumilovesyou.github.io%2Flumiverse%2Fassets%2Fimages%2Fgifypet%2Fbackground.webp&background=&tablecolor=%23f8fdff&textcolor=%23f8fdff" frameborder="0"></iframe>', "https://gifypet.neocities.org/pet/pet.html?name=Mildew&dob=1758063721&gender=undefined&element=Water&pet=https%3A%2F%2Flumilovesyou.github.io%2Flumiverse%2Fassets%2Fimages%2Fgifypet%2Fmildew.gif&map=https%3A%2F%2Flumilovesyou.github.io%2Flumiverse%2Fassets%2Fimages%2Fgifypet%2Fbackground.webp&background=&tablecolor=%23f8fdff&textcolor=%23f8fdff", 20, 20]
+    ]
+    for (let i = 0; i < windowPieces.length; i++) {
+        piece = windowPieces[i]
+        createWindow(piece[0], piece[1], piece[2], piece[3], (window.innerWidth / 100) * piece[4], (window.innerHeight / 100) * piece[5]);
+    }
+
     const windows = document.getElementsByClassName("window");
     for (let i = 0; i < windows.length; i++) {
         makeMouseWindow(windows[i]);
     }
+
 
     const apps = document.getElementsByClassName("app");
     for (let i = 0; i < windows.length; i++) {
@@ -32,12 +40,43 @@ function clamp(value, min, max) {
     }
 }
 
+function createWindow(image, title, contents, source, startX, startY) {
+    // Creates the window
+    const contentBox = document.getElementById("content");
+    let createdWindow = document.createElement("div");
+    createdWindow.classList = `window ${title.toLowerCase()} show`;
+    let header = document.createElement("div");
+    header.classList = "header";
+    let icon = document.createElement("img");
+    icon.src = image;
+    icon.draggable = "false";
+    header.appendChild(icon);
+    header.innerHTML = `${header.innerHTML} ${title}`;
+    createdWindow.appendChild(header);
+    let open = document.createElement("a");
+    open.href = source;
+    open.target = "_blank";
+    open.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width=24 height=24 viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 6h-6a2 2 0 0 0 -2 2v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-6"/><path d="M11 13l9 -9"/><path d="M15 4h5v5"/></svg>';
+    createdWindow.appendChild(open);
+    createdWindow.innerHTML = `${createdWindow.innerHTML}${contents}`;
+    createdWindow.style.left = `${startX}px`;
+    createdWindow.style.top = `${startY}px`;
+    contentBox.appendChild(createdWindow);
+
+    //Creates the app
+    const appBar = document.getElementById("appBar");
+    let app = document.createElement("img");
+    app.src = image;
+    app.draggable = "false";
+    app.classList = `app ${title.toLowerCase()}`;
+    appBar.appendChild(app);
+}
+
 function makeMouseWindow(draggableWindow) {
     const overlay = document.getElementById("overlay");
     let mouseX, mouseY, windowX, windowY = 0
 
     draggableWindow.children[0].onmousedown = startDragWindow;
-    windowPosition(random(0, window.innerWidth - (window.innerWidth / 2)), random(0, window.innerHeight - (window.innerHeight / 2)));
 
     function startDragWindow(event) {
         overlay.classList.toggle("hidden");
@@ -55,7 +94,6 @@ function makeMouseWindow(draggableWindow) {
 
     function moveWindow(event) {
         event.preventDefault();
-        console.log(overlay);
         windowX = mouseX - event.clientX;
         windowY = mouseY - event.clientY;
         mouseX = event.clientX;
