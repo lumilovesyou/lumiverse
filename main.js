633eef847d38ccf254f6fc794bb7d5b98637062d
mewo <177341604+lumilovesyou@users.noreply.github.com>
2026-01-10 17:57:21 -0600

    Noglow and Glow Toggle

--- main.js
+++ main.js
@@ -1,21 +1,30 @@
 const params = new URLSearchParams(document.location.search); if (params.get("unfancy")) {
 
     for (let i = 0; i < document.styleSheets.length; i++) {
-        void(document.styleSheets.item(i).disabled=true)
+        void(document.styleSheets.item(i).disabled=true);
     }
 
 } else {
-
 const embedLink = '<a href="https://lumiverse.dev/" target="_blank"><img src="./assets/images/88x31/lumi-88x31.png">';
 
 document.addEventListener("DOMContentLoaded", () => {
+    //Disable glow
+    const userCard = document.getElementById("userCard");
+    if (params.get("noglow")) {
+        userCard.classList.toggle("dropShadowGlow");
+    }
+
+    document.getElementById("title").addEventListener("click", () => {
+        userCard.classList.toggle("dropShadowGlow");
+    });
+
     //Lumiverse embed 88x31
     let lumi = document.getElementById("lumi-88x31");
     let lumiChild = lumi.lastChild.innerHTML;
     lumi.innerHTML = lumiChild;
     lumi.addEventListener("click", () => {
         navigator.clipboard.writeText(embedLink);
-        alert("Copied embed HTML!")
+        alert("Copied embed HTML!");
     });
 });
 
